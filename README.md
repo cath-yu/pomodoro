b1b819b101ca29b919a39588ba334ea288813fc1
catherineyu0611 <yu359@mcmaster.ca>
2026-04-09 21:56:18 -0400

    basic layout and toggles

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ originally supposed to be piplup themed but matterhorn is too beautiful
 
 # TODO
 
-- [ ] basic website layout
-- [ ] light/dark mode
+- [ x ] basic website layout
+- [ x ] light/dark mode
 - [ ] set timer (25 min work 5 min break)
 - [ ] variable timer
 - [ ] ambient rain
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
 // REACT
 import { useEffect, useState } from "react";
 
+// ASSETS
+import Cloud from "./assets/cloud.svg";
+import MatterhornDark from "./assets/matterhorn_dark.png"
+import MatterhornLight from "./assets/matterhorn_light.jpg";
+import Moon from "./assets/moon.svg";
+import RainCloud from "./assets/rain_cloud.svg";
+import Sun from "./assets/sun.svg";
+
+
 // CSS
 import "./App.css";
 
 export default function App() {
+    const [lightMode, setLightMode] = useState(true);
+    const [rainToggle, setRainToggle] = useState(false);
+    
+    // Switch between backgrounds for light and dark mode
+    useEffect(() => {
+        const background = lightMode ? MatterhornLight : MatterhornDark;
+        document.body.style.backgroundImage = `url(${background})`;
+    }, [lightMode]);
+
+    const handleSiteMode = () => {
+        setLightMode(prev => !prev);
+    }
+
+    const handleRain = () => {
+        setRainToggle(prev => !prev);
+    }
+
     return (
         <div className="main-page">
+            <div className="header">
+                <img src={rainToggle ? Cloud : RainCloud} onClick={handleRain}/>
+                <img src={lightMode ? Moon : Sun} onClick={handleSiteMode}/>
+            </div>
+
+            <div className="body">
+            </div>
         </div>
     )
 }
