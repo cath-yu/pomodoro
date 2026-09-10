6df1f91d6d0a9400203293cd6d4a366249ec37fe
catherineyu0611 <yu359@mcmaster.ca>
2026-09-10 12:59:18 +0800

    added timer and alarm sound effect

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # pomodoro timer
 
-originally supposed to be piplup themed but matterhorn is too beautiful
+originally supposed to be piplup themed but matterhorn is too beautiful so now piplup loves matterhorn
 
 # TODO
 
-- [ x ] basic website layout
-- [ x ] light/dark mode
-- [ ] set timer (25 min work 5 min break)
+- [x] basic website layout
+- [x] light/dark mode
+- [x] set timer (25 min work 5 min break)
+- [x] alarm
 - [ ] variable timer
 - [ ] ambient rain
 - [ ] music?
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,92 @@
 // REACT
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+//
 
 // ASSETS
 import Cloud from "./assets/cloud.svg";
-import MatterhornDark from "./assets/matterhorn_dark.png"
+import MatterhornDark from "./assets/matterhorn_dark.png";
 import MatterhornLight from "./assets/matterhorn_light.jpg";
 import Moon from "./assets/moon.svg";
+import Pause from "./assets/pause.svg";
+import Play from "./assets/play.svg";
 import RainCloud from "./assets/rain_cloud.svg";
 import Sun from "./assets/sun.svg";
-
 
 // CSS
 import "./App.css";
 
 export default function App() {
+    // time consts
+    const workTime = 1500;
+    const breakTime = 300;
+    const longBreakTime = 900;
+
+    // timer states
+    const [time, setTime] = useState(workTime);
+    const [running, setRunning] = useState(false);
+    const [isWorkSession, setIsWorkSession] = useState(true);
+    const [sessionCount, setSessionCount] = useState(0);
+
+    // timer ref and values
+    const timerRef = useRef(null);
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    // visual states
     const [lightMode, setLightMode] = useState(true);
     const [rainToggle, setRainToggle] = useState(false);
+
+    // alarm sound effect
+    const alarmPath = "../public/alarm.mp3";
+    const alarm = new Audio(alarmPath);
+    alarm.volume = 0.08;
     
-    // Switch between backgrounds for light and dark mode
+    // update timer
+    useEffect(() => {
+        if (!running) return;
+
+        timerRef.current = setInterval(() => {
+            // if time is 0, mark as a completed session, otherwise decrement
+            setTime(prev => {
+                if (prev <= 0) {
+                    handleSessionComplete();
+                }
+                return prev-1;
+            });
+        }, 1000);
+
+        return () => clearInterval(timerRef.current);
+    }, [running, isWorkSession])
+
+    // set timer based on the session
+    const handleSessionComplete = () => {
+        // play the alarm sound effect
+        alarm.play();
+
+        // set the next session
+        if (isWorkSession) {
+            // add 1 to a const because of stale state
+            const tempCount = sessionCount+1;
+            setSessionCount(tempCount);
+            // if it is the fourth session, take a longer break
+            setTime(tempCount % 4 === 0 ? longBreakTime : breakTime);
+            setIsWorkSession(false);
+        } else {
+            setTime(workTime);
+            setIsWorkSession(true);
+        }
+    }
+
+    const handleStart = () => {
+        setRunning(true);
+    }
+
+    const handlePause = () => {
+        setRunning(false);
+        clearInterval(timerRef.current);
+    }
+
+    // switch between backgrounds for light and dark mode
     useEffect(() => {
         const background = lightMode ? MatterhornLight : MatterhornDark;
         document.body.style.backgroundImage = `url(${background})`;
@@ -34,11 +103,20 @@ export default function App() {
     return (
         <div className="main-page">
             <div className="header">
-                <img src={rainToggle ? Cloud : RainCloud} onClick={handleRain}/>
-                <img src={lightMode ? Moon : Sun} onClick={handleSiteMode}/>
+                <img src={rainToggle ? Cloud : RainCloud} draggable="false" onClick={handleRain}/>
+                <img src={lightMode ? Moon : Sun} draggable="false" onClick={handleSiteMode}/>
             </div>
-
+            
             <div className="body">
+                <div id="time" className="timer">
+                    {String(minutes).padStart(2, "0")}:
+                    {String(seconds).padStart(2, "0")}
+                </div>
+
+                <div className="icons">
+                    <img src={Play} draggable="false" onClick={handleStart}/>
+                    <img src={Pause} draggable="false" onClick={handlePause}/>
+                </div>
             </div>
         </div>
     )
